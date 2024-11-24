f07b35e3fa93fe356b09cdfd03f6e7f34867b690
Freiric Barral <freiric@gmail.com>
2024-11-24 02:19:17 +0100

    Implement a Fhir DocumentReference POST endpoint to send Document metadata (kdl, patient Id, encounter Nr, creation date) and data to a proprietary API

--- src/main/java/com/example/demo/controller/FhirController.java
+++ src/main/java/com/example/demo/controller/FhirController.java
@@ -1,10 +1,13 @@
 package com.example.demo.controller;
 
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 import org.hl7.fhir.instance.model.api.IPrimitiveType;
+import org.hl7.fhir.r4.model.DocumentReference;
+import org.hl7.fhir.r4.model.Identifier;
 import org.hl7.fhir.r4.model.Patient;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -22,6 +25,7 @@ import ca.uhn.fhir.parser.IParser;
 @RequestMapping // Basis-URL für alle Endpunkte in dieser Klasse
 public class FhirController {
 
+	private static final String DOCUMENT_REFERENCE = "DocumentReference";
 	private static final Logger logger = Logger.getLogger(FhirController.class.getName());
 	private final ProprietaryApiService proprietaryApiService;
 
@@ -73,6 +77,66 @@ public class FhirController {
 		} catch (Exception e) {
 			// Loggt und gibt eine Fehlerantwort zurück, wenn eine Ausnahme auftritt
 			logger.log(Level.SEVERE, "Exception occurred while creating patient", e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error.");
+		}
+	}
+
+	private static final String KDL_SYSTEM = "http://dvmd.de/fhir/CodeSystem/kdl";
+	public static final String ABRECHNUNGSNUMMER = "AN";
+
+	@PostMapping("/" + DOCUMENT_REFERENCE)
+	public ResponseEntity<String> createDocumentReference(@RequestBody String documentReferenceResource) {
+		try {
+			// Loggt die erhaltene Anfrage
+			logger.info("Received request to create document reference: " + documentReferenceResource);
+
+			// Erzeugt einen JSON-Parser für FHIR
+			IParser parser = fhirContext.newJsonParser();
+			// Parsen des Patient-Ressource-Strings in ein Patient-Objekt
+			DocumentReference documentReference = parser.parseResource(DocumentReference.class,
+					documentReferenceResource);
+
+			// Extrahieren des KDL-Code aus der DocumentReference-Ressource
+			String kdlCode = documentReference.getType().getCoding().stream()
+					.filter(coding -> KDL_SYSTEM.equals(coding.getSystem())).findFirst().get().getCode();
+			// Extrahieren des Patienten-ID aus der DocumentReference-Ressource
+			String patientenId = documentReference.getSubject().getIdentifier().getValue();
+			// Extrahieren des Abrechnungsfall-Nummer aus der DocumentReference-Ressource
+			String abrechnungsfallNummer = documentReference.getContext().getEncounter().stream().filter(reference -> {
+				if (reference.getIdentifier() == null)
+					return false;
+				Identifier identifier = reference.getIdentifier();
+				return ABRECHNUNGSNUMMER.equals(identifier.getSystem());
+			}).findFirst().get().getIdentifier().getValue();
+			// Extrahieren des Erstelldatum aus der DocumentReference-Ressource
+			Date creationDate = documentReference.getContent().get(0).getAttachment().getCreation();
+			// Extrahieren der Dokument data
+			byte[] data = documentReference.getContent().get(0).getAttachment().getData();
+
+			// Loggt die extrahierten und konvertierten Patientendaten
+			logger.info("Parsed document data: KDL code: " + kdlCode + ", patienten Id: " + patientenId
+					+ ", abrechnungsfallNummer: " + abrechnungsfallNummer + ", creationDate: " + creationDate
+					+ ", data:" + data);
+
+			int patientenIdInt = Integer.parseInt(patientenId);
+			int abrechnungsfallNummerInt = Integer.parseInt(abrechnungsfallNummer);
+			// Sendet die Dokumentdaten an die proprietäre API
+			boolean apiSuccess = proprietaryApiService.sendDocumentData(kdlCode, patientenIdInt,
+					abrechnungsfallNummerInt, creationDate, data);
+
+			if (apiSuccess) {
+				// Loggt und gibt eine Erfolgsantwort zurück, wenn die API-Anfrage erfolgreich
+				// war
+				logger.info("document data sent successfully.");
+				return ResponseEntity.status(HttpStatus.CREATED).body("Document created successfully.");
+			} else {
+				// Loggt und gibt eine Fehlerantwort zurück, wenn die API-Anfrage fehlschlägt
+				logger.severe("Failed to send document data to proprietary API.");
+				return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error.");
+			}
+		} catch (Exception e) {
+			// Loggt und gibt eine Fehlerantwort zurück, wenn eine Ausnahme auftritt
+			logger.log(Level.SEVERE, "Exception occurred while creating document", e);
 			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Internal Server Error.");
 		}
 	}
